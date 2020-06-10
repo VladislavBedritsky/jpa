d8b4e241861055e2c30cc923f3f4f10f81523365
Vladislav Bedritsky <vladquinn2010@gmail.com>
2020-06-10 19:52:26 +0300

    update app

--- src/main/java/org/example/domain/Customer.java
+++ src/main/java/org/example/domain/Customer.java
@@ -1,13 +1,11 @@
 package org.example.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name="customers")
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
